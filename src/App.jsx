b92ab705b7ad16e08d75b897163b7f1badd891b4
Sahil Master <7882mastersahil@gmail.com>
2025-02-25 14:01:33 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Weather from './components/WeatherScreen'
+
+function App() {
+
+  return (
+    <>
+    <div className="main">
+      <Weather />
+    </div>
+    </>
+  )
+}
+
+export default App
